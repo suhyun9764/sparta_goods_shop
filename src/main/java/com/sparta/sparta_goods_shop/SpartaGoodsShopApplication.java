44dd2db5f1ce1926d0dbf213c367f8e5304f3c84
suhyun9764 <suhyun9764@naver.com>
2024-03-13 02:43:11 +0900

    build(): init commit

---
+++ src/main/java/com/sparta/sparta_goods_shop/SpartaGoodsShopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_goods_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaGoodsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaGoodsShopApplication.class, args);
+    }
+
+}
